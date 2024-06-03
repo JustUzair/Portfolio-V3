64b623cf06af403b21102dd27305f9a7bb62e193
JustUzair <uzairhajra763390@gmail.com>
2024-06-04 00:22:50 +0530

    Vercel Deploy Fix

--- components/ProjectTabs.tsx
+++ components/ProjectTabs.tsx
@@ -80,11 +80,8 @@ const Projects = ({ filterBy }: { filterBy: string }) => {
 
         if (item.category.includes(filterBy) || filterBy === "all")
           return (
-            <Link href={item.link} target="_blank">
-              <div
-                className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-                key={item.id}
-              >
+            <Link href={item.link} target="_blank" key={item.id}>
+              <div className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
                 <PinContainer title={item.link} href={item.link}>
                   <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                     <div
