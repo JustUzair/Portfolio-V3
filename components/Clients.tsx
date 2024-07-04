85e023244d6876da9aef1b6a3b76281cf921cde6
JustUzair <uzairhajra763390@gmail.com>
2024-07-04 14:25:38 +0530

    vercel deployment fixes

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -25,25 +25,27 @@ const Clients = () => {
           />
         </div>
 
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
-          {companies.map((company) => (
-            <React.Fragment key={company.id}>
-              <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
-                  src={company.img}
-                  alt={company.name}
-                  className="md:w-24 w-20"
-                />
-                {/* <img
+        {/* <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+          {companies &&
+            companies.length > 0 &&
+            companies.map((company) => (
+              <React.Fragment key={company.id}>
+                <div className="flex md:max-w-60 max-w-32 gap-2">
+                  <img
+                    src={company.img}
+                    alt={company.name}
+                    className="md:w-24 w-20"
+                  />
+                  {/* <img
                   src={company.nameImg}
                   alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
                 /> */}
-              </div>
-            </React.Fragment>
-          ))}
-        </div>
+                </div>
+              </React.Fragment>
+            ))}
+        </div> */}
       </div>
     </section>
   );
